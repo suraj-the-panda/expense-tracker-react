d9916f55bf8eed8917af69363589a17c99e1a849
Suraj Pandey <suraj@volumetree.com>
2022-07-26 10:31:38 +0530

    updated with New components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h1>{props.title}</h1>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
